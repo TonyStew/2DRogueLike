ad29fd51f9cf9be43fbfa509884e73f1b5257b11
TonyStew <bino4444@gmail.com>
2016-10-31 19:29:31 -0700

    upload

---
+++ Assets/Completed/Scripts/Bat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed {
+    public class Bat : Enemy
+    {
+        //Overriding the AttemptMove function of the parent (Enemy) so that it moves every turn rather than every two.
+        protected override void AttemptMove<T>(int xDir, int yDir)
+        {
+            //telling parent not to skip move
+            setSkipMove(false);
+            //Call the AttemptMove function from MovingObject.
+            base.AttemptMove<T>(xDir, yDir);
+        }
+
+    }
+}
